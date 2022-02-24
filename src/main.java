d7069c03b2e2e384ea23d0cf8f242dff7996fbae
Zvoni004 <zvonimir.ljubic@htl-saalfelden.at>
2022-02-24 11:54:08 +0100

    zumwegwerfen2

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("Hallo :D");
+        System.out.println("Hallo :D");
     }
 }
